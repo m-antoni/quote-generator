8e2409354a27af85b563b743516c38ad3e2f55ed
Michael Antoni <michaelantoni.cs@gmail.com>
2021-01-14 00:23:12 +0800

    minimal updates

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -11,15 +11,15 @@ interface Props {
 const Main: React.FC<Props> = ({ quote, generateQuote, copyToClipboard, isCopy }) => {
     return (
         <div className="container">
-            {
-                isCopy && <div className="copy-message"><i className="fa fa-check"></i> Quote Copied!</div>
-            }
-            <button onClick={generateQuote} className="btn-quote">GET QUOTE</button>
+            { isCopy && <div className="copy-message"><i className="fa fa-check"></i> Quote Copied!</div>  }
+
             <div className="q-container">
                 <div className="copy-btn" onClick={!isCopy ? copyToClipboard : () => {}}><i className="fa fa-copy"></i></div>
                 <div className="q-text">"{quote.text}"</div>
                 <div className="q-author">~ {quote.author ? quote.author : 'Unknown'} ~</div>
             </div>
+
+            <button onClick={generateQuote} className="btn-quote">GET QUOTE</button>
         </div>
     )
 }
